2d4d9161a193a658b9c50883069219791799ce2d
Ribeiro <lucasribeiro1024b@gmail.com>
2022-02-28 12:30:13 -0300

    work: update JS, interval

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
 function timer() {
   var time;
-  if (this.id == 'shortRestButton'){
+  if (this.id == "shortRestButton"){
     time = 45;
   }
+  if (this.id == "longRestButton"){
+    time = 180;
+  }
 
-  interval = setInterval(function (limit) {
-    console.log(time);
+  interval = setInterval(function () {
+    timerValue.innerHTML = time;
     time -= 1;
 
     if (time < 0) {
@@ -14,7 +17,7 @@ function timer() {
   }, 1000);
 }
 
-timerValue = document.getElementById("timerNumber").innerHTML;
+timerValue = document.getElementById("timerNumber");
 
 shortRestButon = document.getElementById("shortRestButton");
 longRestButon = document.getElementById("longRestButton");
